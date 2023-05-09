023ccc2d18ad8b1b0a3fc73ec5f98408fb8cfe76
Bobbys-Builders <43325697+Bobbys-Builders@users.noreply.github.com>
2023-05-09 15:28:48 -0700

    Create Pair.java

---
+++ Pair.java
@@ -0,0 +1,22 @@
+  public class Pair {
+    private int x;
+    private int y;
+
+    public Pair() {
+      this.x = 0;
+      this.y = 0;
+    }
+
+    public Pair(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    public int getX() {
+      return x;
+    }
+
+    public int getY() {
+      return y;
+    }
+  }
