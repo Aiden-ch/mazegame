ceea06b065f0e2253f890e983e1311b297f26249
Bobbys-Builders <43325697+Bobbys-Builders@users.noreply.github.com>
2023-05-10 10:46:49 -0700

    Made a working prototype of the maze navigation

--- Mazegame.java
+++ Mazegame.java
@@ -2,26 +2,29 @@ import java.util.*;
 
 public class Mazegame {
   
-  private bool w = false;
-  private bool a = false;
-  private bool s = false;
-  private bool d = false;
+  private static char[][] maze;
+  
+//   private bool w = false;
+//   private bool a = false;
+//   private bool s = false;
+//   private bool d = false;
   
   // map variables
-  private int xPos = 49;
-  private int yPos = 49;
+  private static int xPos = 49;
+  private static int yPos = 49;
+  private static Scanner sc = new Scanner(System.in);
   
   // fight variables
-  private double xPosFight = 7.0;
-  private double yPosFight = 7.0;
-  private double xVelFight = 0.0;
-  private double yVelFight = 0.0;
+//   private double xPosFight = 7.0;
+//   private double yPosFight = 7.0;
+//   private double xVelFight = 0.0;
+//   private double yVelFight = 0.0;
   
   // buffs
-  private double speedModifier = 1;
+//   private double speedModifier = 1;
   
-  private bool inInventory = false;
-  private bool inFight = false;
+//   private bool inInventory = false;
+//   private bool inFight = false;
   
   public static double abs(double a) {
     if (a >= 0) return a;
@@ -44,85 +47,132 @@ public class Mazegame {
   }
   
   public static void mapState() {
-    int xVel = 0;
-    int yVel = 0;
-    if 'w' pressed && !w
-      w = true;
-      yVel++;
-    else
-      w = false
-    if 'a' pressed && !a
-      a = true;
-      xVel--;
-    else
-      a = false
-    if 's' pressed && !s
-      s = true;
-      yVel--;
-    else
-      s = false
-    if 'd' pressed && !d
-      d = true;
-      xVel++;
-    else
-      d = false
+//     int xVel = 0;
+//     int yVel = 0;
+//     if 'w' pressed && !w
+//       w = true;
+//       yVel++;
+//     else
+//       w = false
+//     if 'a' pressed && !a
+//       a = true;
+//       xVel--;
+//     else
+//       a = false
+//     if 's' pressed && !s
+//       s = true;
+//       yVel--;
+//     else
+//       s = false
+//     if 'd' pressed && !d
+//       d = true;
+//       xVel++;
+//     else
+//       d = false
 
-    xPos += xVel;
-    yPos += yVel;
+    
+//     xPos += xVel;
+//     yPos += yVel;
     
     // display stuff
+    
+    
+    char c = sc.next().charAt(0);
+    switch (c) {
+      case 'w':
+        if (maze[yPos - 1][xPos] != '#') {
+          yPos--;
+        }
+        break;
+      case 'a':
+        if (maze[yPos][xPos - 1] != '#') {
+          xPos--;
+        }
+        break;
+      case 's':
+        if (maze[yPos + 1][xPos] != '#') {
+          yPos++;
+        }
+        break;
+      case 'd':
+        if (maze[yPos][xPos + 1] != '#') {
+          xPos++;
+        }
+        break;
+    }
   }
   
-  public static void fightState() {
-    int xVel = 0;
-    int yVel = 0;
-    if 'w' pressed
-      w = true;
-      yVel++;
-    else
-      w = false
-    if 'a' pressed
-      a = true;
-      xVel--;
-    else
-      a = false
-    if 's' pressed
-      s = true;
-      yVel--;
-    else
-      s = false
-    if 'd' pressed
-      d = true;
-      xVel++;
-    else
-      d = false
+//   public static void fightState() {
+//     int xVel = 0;
+//     int yVel = 0;
+//     if 'w' pressed
+//       w = true;
+//       yVel++;
+//     else
+//       w = false
+//     if 'a' pressed
+//       a = true;
+//       xVel--;
+//     else
+//       a = false
+//     if 's' pressed
+//       s = true;
+//       yVel--;
+//     else
+//       s = false
+//     if 'd' pressed
+//       d = true;
+//       xVel++;
+//     else
+//       d = false
 
-    xVelFight += sign(xVelFight - xVel) * min(0.1, xVelFight - xVel);
-    yVelFight += sign(yVelFight - yVel) * min(0.1, yVelFight - yVel);
-    xPosFight += xVelFight * speedModifier;
-    yPosFight += yVelFight * speedModifier;
+//     xVelFight += sign(xVelFight - xVel) * min(0.1, xVelFight - xVel);
+//     yVelFight += sign(yVelFight - yVel) * min(0.1, yVelFight - yVel);
+//     xPosFight += xVelFight * speedModifier;
+//     yPosFight += yVelFight * speedModifier;
 
-    if clicking
-      do specified weapon attack in mouse direction
-      vector: <mouseX - xPosFight, mouseY - yPosFight>
+//     if clicking
+//       do specified weapon attack in mouse direction
+//       vector: <mouseX - xPosFight, mouseY - yPosFight>
     
-    // display stuff
-  }
+//     // display stuff
+//   }
   
   public static void main(String[] args) {
     
+    maze = Generator.generateMaze();
+    char[][] visibleMaze = new char[99][99];
+    for (int i = 0; i < 99; i++) { 
+      for (int o = 0; o < 99; o++) {
+        visibleMaze[i][o] = '*';
+      }
+    }
     // initialization stuff
     
     while (true) {
-      if (!inInventory) {
-        if (!inFight) {
-          mapState();
-        } else {
-          fightState();
+      if (maze[yPos][xPos] == 'e') return;
+//       if (!inInventory) {
+//         if (!inFight) {
+      visibleMaze[yPos][xPos] = maze[yPos][xPos];
+      visibleMaze[yPos + 1][xPos] = maze[yPos + 1][xPos];
+      visibleMaze[yPos - 1][xPos] = maze[yPos - 1][xPos];
+      visibleMaze[yPos][xPos + 1] = maze[yPos][xPos + 1];
+      visibleMaze[yPos][xPos - 1] = maze[yPos][xPos - 1];
+      for (int i = yPos - 4; i <= yPos + 4; i++) {
+        for (int o = xPos - 4; o <= xPos + 4; o++) {
+          if (i == yPos && o == xPos) System.out.print("@ ");
+          else if (i < 0 || i > 98 || o < 0 || o > 98) System.out.print("* ");
+          else System.out.print(visibleMaze[i][o] + " ");
         }
-      } else {
-        // inventory interface
+        System.out.println();
       }
+      mapState();
+//         } else {
+//           fightState();
+//         }
+//       } else {
+        // inventory interface
+//       }
     }
     
   }
