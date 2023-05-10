0a91e40042bd5ebfdc841591531c04a9986b665b
Bobbys-Builders <43325697+Bobbys-Builders@users.noreply.github.com>
2023-05-09 17:39:42 -0700

    Hopefully fixed connectivity

--- Generator.java
+++ Generator.java
@@ -5,33 +5,33 @@ public class Generator {
     private static char[][] maze = new char[99][99];
     private static boolean[][] visited = new boolean[99][99];;
 
-private static char[][] start = {{' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
-                                 {' ', '#', ' ', ' ', ' ', '#', ' '}, 
-                                 {' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
-                                 {' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
-                                 {' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
-                                 {' ', '#', ' ', ' ', ' ', '#', ' '}, 
-                                 {' ', ' ', ' ', ' ', ' ', ' ', ' '}};
+    private static char[][] start = {{' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
+                                     {' ', '#', ' ', ' ', ' ', '#', ' '}, 
+                                     {' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
+                                     {' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
+                                     {' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
+                                     {' ', '#', ' ', ' ', ' ', '#', ' '}, 
+                                     {' ', ' ', ' ', ' ', ' ', ' ', ' '}};
 
-private static char[][] boss = {{'#', '#', '#', '#', ' ', '#', '#', '#', '#'}, 
-                                {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
-                                {'#', ' ', '#', ' ', ' ', ' ', '#', ' ', '#'}, 
-                                {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
-                                {' ', ' ', ' ', ' ', '$', ' ', ' ', ' ', ' '}, 
-                                {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
-                                {'#', ' ', '#', ' ', ' ', ' ', '#', ' ', '#'}, 
-                                {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
-                                {'#', '#', '#', '#', ' ', '#', '#', '#', '#'}};
+    private static char[][] boss = {{'#', '#', '#', '#', ' ', '#', '#', '#', '#'}, 
+                                    {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+                                    {'#', ' ', '#', ' ', ' ', ' ', '#', ' ', '#'}, 
+                                    {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+                                    {' ', ' ', ' ', ' ', '$', ' ', ' ', ' ', ' '}, 
+                                    {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+                                    {'#', ' ', '#', ' ', ' ', ' ', '#', ' ', '#'}, 
+                                    {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+                                    {'#', '#', '#', '#', ' ', '#', '#', '#', '#'}};
 
-private static char[][] end = {{'#', '#', '#', '#', ' ', '#', '#', '#', '#'}, 
-                               {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
-                               {'#', ' ', '#', ' ', ' ', ' ', '#', ' ', '#'}, 
-                               {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
-                               {' ', ' ', ' ', ' ', 'e', ' ', ' ', ' ', '#'}, 
-                               {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
-                               {'#', ' ', '#', ' ', ' ', ' ', '#', ' ', '#'}, 
-                               {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
-                               {'#', '#', '#', '#', '#', '#', '#', '#', '#'}};
+    private static char[][] end = {{'#', '#', '#', '#', ' ', '#', '#', '#', '#'}, 
+                                   {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+                                   {'#', ' ', '#', ' ', ' ', ' ', '#', ' ', '#'}, 
+                                   {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+                                   {' ', ' ', ' ', ' ', 'e', ' ', ' ', ' ', '#'}, 
+                                   {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+                                   {'#', ' ', '#', ' ', ' ', ' ', '#', ' ', '#'}, 
+                                   {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+                                   {'#', '#', '#', '#', '#', '#', '#', '#', '#'}};
                                
     public static int min(int a, int b) {
         if (a < b) return a;
@@ -60,19 +60,19 @@ private static char[][] end = {{'#', '#', '#', '#', ' ', '#', '#', '#', '#'},
             maze[curPoint.getY()][curPoint.getX()] = ' ';
             
             if (!visited[min(97, curPoint.getY() + 1)][curPoint.getX()]) {
-                if (Math.random() < 0.65) bfs.add(new Pair(curPoint.getX(), curPoint.getY() + 1));
+                if (Math.random() < 0.65 || maze[curPoint.getY() + 1][curPoint.getX()] == ' ') bfs.add(new Pair(curPoint.getX(), curPoint.getY() + 1));
                 visited[curPoint.getY() + 1][curPoint.getX()] = true;
             }
             if (!visited[max(1, curPoint.getY() - 1)][curPoint.getX()]) {
-                if (Math.random() < 0.65) bfs.add(new Pair(curPoint.getX(), curPoint.getY() - 1));
+                if (Math.random() < 0.65 || maze[curPoint.getY() - 1][curPoint.getX()] == ' ') bfs.add(new Pair(curPoint.getX(), curPoint.getY() - 1));
                 visited[curPoint.getY() - 1][curPoint.getX()] = true;
             }
             if (!visited[curPoint.getY()][min(97, curPoint.getX() + 1)]) {
-                if (Math.random() < 0.65) bfs.add(new Pair(curPoint.getX() + 1, curPoint.getY()));
+                if (Math.random() < 0.65 || maze[curPoint.getY()][curPoint.getX() + 1] == ' ') bfs.add(new Pair(curPoint.getX() + 1, curPoint.getY()));
                 visited[curPoint.getY()][curPoint.getX() + 1] = true;
             }
             if (!visited[curPoint.getY()][max(1, curPoint.getX() - 1)]) {
-                if (Math.random() < 0.65) bfs.add(new Pair(curPoint.getX() - 1, curPoint.getY()));
+                if (Math.random() < 0.65 || maze[curPoint.getY()][curPoint.getX() - 1] == ' ') bfs.add(new Pair(curPoint.getX() - 1, curPoint.getY()));
                 visited[curPoint.getY()][curPoint.getX() - 1] = true;
             }
         }
@@ -111,25 +111,24 @@ private static char[][] end = {{'#', '#', '#', '#', ' ', '#', '#', '#', '#'},
     }
     
     public static char[][] generateMaze() {
-        for (int i = 0; i < 99; i++) {
-            for (int o = 0; o < 99; o++) {
-                maze[o][i] = '#';
-            }
-        }
-        
-        fill(0, 0, boss);
-        fill(0, 90, boss);
-        fill(90, 0, boss);
-        fill(90, 90, end);
-        fill(46, 46, start); // fill the start section + boss rooms
-        for (int i = 0; i < 99; i++) {
-          maze[i][0] = '#';
-          maze[0][i] = '#';
-          maze[i][98] = '#';
-          maze[98][i] = '#';
-        }
-        
         while (checkContinuity(new Pair(48, 48), 0) < 4) {
+            for (int i = 0; i < 99; i++) {
+                for (int o = 0; o < 99; o++) {
+                    maze[o][i] = '#';
+                }
+            }
+
+            fill(0, 0, boss);
+            fill(0, 90, boss);
+            fill(90, 0, boss);
+            fill(90, 90, end);
+            fill(46, 46, start); // fill the start section + boss rooms
+            for (int i = 0; i < 99; i++) {
+              maze[i][0] = '#';
+              maze[0][i] = '#';
+              maze[i][98] = '#';
+              maze[98][i] = '#';
+            }
             generateTunnels(); // somehow fill other portions(maybe drill tunnels using bfs)
             visited = new boolean[99][99];
         }
@@ -139,15 +138,15 @@ private static char[][] end = {{'#', '#', '#', '#', ' ', '#', '#', '#', '#'},
         return maze;
     }
     
-    public static void main(String[] args) {
-        generateMaze();
+//     public static void main(String[] args) {
+//         generateMaze();
         
-        for (int i = 0; i < 99; i++) {
-            for (int o = 0; o < 99; o++) {
-                System.out.print(maze[o][i] + "");
-            }
-            System.out.println();
-        }
-    }
+//         for (int i = 0; i < 99; i++) {
+//             for (int o = 0; o < 99; o++) {
+//                 System.out.print(maze[o][i] + "");
+//             }
+//             System.out.println();
+//         }
+//     }
 
 }
