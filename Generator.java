3cfecc2cc58e4585884d74732b5b3ed54037b79e
Bobbys-Builders <43325697+Bobbys-Builders@users.noreply.github.com>
2023-05-11 14:28:07 -0700

    Changed maze size

--- Generator.java
+++ Generator.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 
 public class Generator {
-    private static char[][] maze = new char[99][99];
-    private static boolean[][] visited = new boolean[99][99];;
+    private static char[][] maze = new char[59][59];
+    private static boolean[][] visited = new boolean[59][59];;
 
     private static char[][] start = {{' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
                                      {' ', '#', ' ', ' ', ' ', '#', ' '}, 
@@ -43,13 +43,13 @@ public class Generator {
     
     public static void generateTunnels() {
         Queue<Pair> bfs = new LinkedList<>();
-        bfs.add(new Pair(48, 48));
+        bfs.add(new Pair(28, 28));
         
         while(bfs.size() > 0) {
             Pair curPoint = bfs.poll();
             maze[curPoint.getY()][curPoint.getX()] = ' ';
             
-            if (!visited[min(97, curPoint.getY() + 1)][curPoint.getX()]) {
+            if (!visited[min(57, curPoint.getY() + 1)][curPoint.getX()]) {
                 if (Math.random() < 0.63 || maze[curPoint.getY() + 1][curPoint.getX()] == ' ') bfs.add(new Pair(curPoint.getX(), curPoint.getY() + 1));
                 visited[curPoint.getY() + 1][curPoint.getX()] = true;
             }
@@ -57,7 +57,7 @@ public class Generator {
                 if (Math.random() < 0.63 || maze[curPoint.getY() - 1][curPoint.getX()] == ' ') bfs.add(new Pair(curPoint.getX(), curPoint.getY() - 1));
                 visited[curPoint.getY() - 1][curPoint.getX()] = true;
             }
-            if (!visited[curPoint.getY()][min(97, curPoint.getX() + 1)]) {
+            if (!visited[curPoint.getY()][min(57, curPoint.getX() + 1)]) {
                 if (Math.random() < 0.63 || maze[curPoint.getY()][curPoint.getX() + 1] == ' ') bfs.add(new Pair(curPoint.getX() + 1, curPoint.getY()));
                 visited[curPoint.getY()][curPoint.getX() + 1] = true;
             }
@@ -74,21 +74,21 @@ public class Generator {
             i++;
         }
         i = 9;
-        while (maze[i][94] == '#' && maze[4][98 - i] == '#') {
-            maze[i][94] = ' ';
-            maze[4][98 - i] = ' ';
+        while (maze[i][54] == '#' && maze[4][58 - i] == '#') {
+            maze[i][54] = ' ';
+            maze[4][58 - i] = ' ';
             i++;
         }
         i = 9;
-        while (maze[98 - i][4] == '#' && maze[94][i] == '#') {
-            maze[98 - i][4] = ' ';
-            maze[94][i] = ' ';
+        while (maze[58 - i][4] == '#' && maze[54][i] == '#') {
+            maze[58 - i][4] = ' ';
+            maze[54][i] = ' ';
             i++;
         }
         i = 9;
-        while (maze[98 - i][94] == '#' && maze[94][98 - i] == '#') {
-            maze[98 - i][94] = ' ';
-            maze[94][98 - i] = ' ';
+        while (maze[58 - i][54] == '#' && maze[54][58 - i] == '#') {
+            maze[58 - i][54] = ' ';
+            maze[54][58 - i] = ' ';
             i++;
         }
     }
@@ -112,29 +112,29 @@ public class Generator {
     }
     
     public static char[][] generateMaze() {
-        for (int i = 0; i < 99; i++) {
-            for (int o = 0; o < 99; o++) {
+        for (int i = 0; i < 59; i++) {
+            for (int o = 0; o < 59; o++) {
                 maze[o][i] = '#';
             }
         }
 
         fill(0, 0, boss);
-        fill(0, 90, boss);
-        fill(90, 0, boss);
-        fill(90, 90, boss);
-        maze[4 + (int)(Math.random() * 2) * 90][4 + (int)(Math.random() * 2) * 90] = 'e';
-        fill(46, 46, start); // fill the start section + boss rooms
-        for (int i = 0; i < 99; i++) {
+        fill(0, 50, boss);
+        fill(50, 0, boss);
+        fill(50, 90, boss);
+        maze[4 + (int)(Math.random() * 2) * 50][4 + (int)(Math.random() * 2) * 50] = 'e';
+        fill(26, 26, start); // fill the start section + boss rooms
+        for (int i = 0; i < 59; i++) {
           maze[i][0] = '#';
           maze[0][i] = '#';
-          maze[i][98] = '#';
-          maze[98][i] = '#';
+          maze[i][58] = '#';
+          maze[58][i] = '#';
         }
         
         generateTunnels(); // somehow fill other portions(maybe drill tunnels using bfs)
         
-        visited = new boolean[99][99];
-        generateChests(new Pair(47, 47)); // do dfs and populate chests/open new passages???
+        visited = new boolean[59][59];
+        generateChests(new Pair(28, 28)); // do dfs and populate chests/open new passages???
         
         return maze;
     }
