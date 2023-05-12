77351eccaf648b774d5716cb84200f1951545a51
Aiden-ch <115767172+Aiden-ch@users.noreply.github.com>
2023-05-12 15:11:15 -0700

    maze combat so far
    projectiles fire right
    - need to make screen fullscreen from start
    
    melee still is causing me grief

---
+++ game/ItemHandler.java
@@ -0,0 +1,30 @@
+package com.mygdx.game;
+
+import java.util.ArrayList;
+
+//import com.badlogic.gdx.graphics.Texture;
+//import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class ItemHandler {
+	//store all items
+	static ArrayList<Item> allItems;
+	ArrayList<Item> collected;
+	
+	public ItemHandler() {
+		allItems = new ArrayList<Item>();
+		collected = new ArrayList<Item>();
+	}
+	
+	public void addItem(String name, Projectile proj) {
+		Item bombCard = new Item(name, proj);
+		allItems.add(bombCard);
+	}
+	public void addItem(String name, Melee mel) {
+		Item bombCard = new Item(name, mel);
+		allItems.add(bombCard);
+	}
+	
+	public void testing() {
+		collected = allItems;
+	}
+}
